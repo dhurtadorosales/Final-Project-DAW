d6ff9ad0819903f28cbbfdad725b9f595d9361ca
diego <dhurtadorosales@gmail.com>
2017-03-23 23:54:09 +0100

    Jquery para menú hover

---
+++ web/script/script.js
@@ -0,0 +1,11 @@
+$(function () {
+   main();
+});
+
+function main() {
+    $('#menu').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });
+}
