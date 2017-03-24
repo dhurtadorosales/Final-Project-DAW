7cc88e484bdf636c03c48f9440690aef9727424a
diego <dhurtadorosales@gmail.com>
2017-03-24 02:13:27 +0100

    Añadido carrusel de imágenes

--- web/script/script.js
+++ web/script/script.js
@@ -3,7 +3,7 @@ $(function () {
 });
 
 function main() {
-    $('#menu').hover(function() {
+    $('.desplegable').hover(function() {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
     }, function() {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
