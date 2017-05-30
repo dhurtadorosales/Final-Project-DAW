74305d69e59fdf9f7dee58f5a40ddc45cba6853d
diego <dhurtadorosales@gmail.com>
2017-05-30 21:37:57 +0200

    Activadas traducciones en el resto de páginas. Se traduce solo lampante

--- src/AppBundle/Controller/PaginaController.php
+++ src/AppBundle/Controller/PaginaController.php
@@ -12,169 +12,49 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 class PaginaController extends Controller
 {
     /**
-     * @Route("/virgen/extra", name="virgen_extra")
-     *
+     * @Route("/virgen/extra", name="virgen_extra", defaults={"_locale"="es"}, requirements={"_locale"="%app.locales%"})
+     * @Route("/virgen/extra/{_locale}", name="virgen_extra_locale", requirements={"_locale" = "%app.locales%"})
+     * @Route("/virgen", name="virgen", defaults={"_locale"="es"}, requirements={"_locale"="%app.locales%"})
+     * @Route("/virgen/{_locale}", name="virgen_locale", requirements={"_locale" = "%app.locales%"})
+     * @Route("/lampante", name="lampante", defaults={"_locale"="es"}, requirements={"_locale"="%app.locales%"})
+     * @Route("/lampante/{_locale}", name="lampante_locale", requirements={"_locale" = "%app.locales%"})
+     * @Route("/picual", name="picual", defaults={"_locale"="es"}, requirements={"_locale"="%app.locales%"})
+     * @Route("/picual/{_locale}", name="picual_locale", requirements={"_locale" = "%app.locales%"})
+     * @Route("/alberquina", name="alberquina", defaults={"_locale"="es"}, requirements={"_locale"="%app.locales%"})
+     * @Route("/alberquina/{_locale}", name="alberquina_locale", requirements={"_locale" = "%app.locales%"})
+     * @Route("/gordal", name="gordal", defaults={"_locale"="es"}, requirements={"_locale"="%app.locales%"})
+     * @Route("/gordal/{_locale}", name="gordal_locale", requirements={"_locale" = "%app.locales%"})
+     * @Route("/hojiblanca", name="hojiblanca", defaults={"_locale"="es"}, requirements={"_locale"="%app.locales%"})
+     * @Route("/hojiblanca/{_locale}", name="hojiblanca_locale", requirements={"_locale" = "%app.locales%"})
+     * @Route("/lechin", name="lechin", defaults={"_locale"="es"}, requirements={"_locale"="%app.locales%"})
+     * @Route("/lechin/{_locale}", name="lechin_locale", requirements={"_locale" = "%app.locales%"})
      */
-    public function virgenExtraAction(Request $request)
+    public function indexAction(Request $request)
     {
         if ($this->get('security.authorization_checker')->isGranted('ROLE_USUARIO')) {
             return $this->render('default/principal.html.twig', [
                 'temporadas' => false
-            ]);
-        } else {
-
-            $this->get('translator')->trans('layout.inicio');
-
-            return $this->render('pagina/virgen_extra.html.twig', [
-                'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-                'locale' => $request->getLocale(),
             ]);
         }
-    }
+        else {
+            //ruta actual
+            $ruta = $request->get('_route');
 
-    /**
-     * @Route("/virgen", name="virgen")
-     *
-     */
-    public function virgenAction(Request $request)
-    {
-        if ($this->get('security.authorization_checker')->isGranted('ROLE_USUARIO')) {
-            return $this->render('default/principal.html.twig', [
-                'temporadas' => false
-            ]);
-        } else {
+            //Se comprueba que contiene la subcadena _locale
+            if (strpos($ruta, '_locale') != false) {
+                $ruta = str_replace('_locale', '', $ruta);
+            }
+
+            $titulo = 'pagina.titulo_' . $ruta;
+            $cuerpo = 'pagina.cuerpo_' . $ruta;
 
             $this->get('translator')->trans('layout.inicio');
 
-            return $this->render('pagina/virgen.html.twig', [
+            return $this->render('pagina/pagina.html.twig', [
                 'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
                 'locale' => $request->getLocale(),
-            ]);
-        }
-    }
-
-    /**
-     * @Route("/lampante", name="lampante")
-     *
-     */
-    public function lampanteAction(Request $request)
-    {
-        if ($this->get('security.authorization_checker')->isGranted('ROLE_USUARIO')) {
-            return $this->render('default/principal.html.twig', [
-                'temporadas' => false
-            ]);
-        } else {
-
-            $this->get('translator')->trans('layout.inicio');
-
-            return $this->render('pagina/lampante.html.twig', [
-                'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-                'locale' => $request->getLocale(),
-            ]);
-        }
-    }
-
-    /**
-     * @Route("/picual", name="picual")
-     *
-     */
-    public function picualAction(Request $request)
-    {
-        if ($this->get('security.authorization_checker')->isGranted('ROLE_USUARIO')) {
-            return $this->render('default/principal.html.twig', [
-                'temporadas' => false
-            ]);
-        } else {
-
-            $this->get('translator')->trans('layout.inicio');
-
-            return $this->render('pagina/picual.html.twig', [
-                'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-                'locale' => $request->getLocale(),
-            ]);
-        }
-    }
-
-    /**
-     * @Route("/alberquina", name="alberquina")
-     *
-     */
-    public function alberquinaAction(Request $request)
-    {
-        if ($this->get('security.authorization_checker')->isGranted('ROLE_USUARIO')) {
-            return $this->render('default/principal.html.twig', [
-                'temporadas' => false
-            ]);
-        } else {
-
-            $this->get('translator')->trans('layout.inicio');
-
-            return $this->render('pagina/alberquina.html.twig', [
-                'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-                'locale' => $request->getLocale(),
-            ]);
-        }
-    }
-
-    /**
-     * @Route("/gordal", name="gordal")
-     *
-     */
-    public function gordalAction(Request $request)
-    {
-        if ($this->get('security.authorization_checker')->isGranted('ROLE_USUARIO')) {
-            return $this->render('default/principal.html.twig', [
-                'temporadas' => false
-            ]);
-        } else {
-
-            $this->get('translator')->trans('layout.inicio');
-
-            return $this->render('pagina/gordal.html.twig', [
-                'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-                'locale' => $request->getLocale(),
-            ]);
-        }
-    }
-
-    /**
-     * @Route("/hojiblanca", name="hojiblanca")
-     *
-     */
-    public function hojiblancaAction(Request $request)
-    {
-        if ($this->get('security.authorization_checker')->isGranted('ROLE_USUARIO')) {
-            return $this->render('default/principal.html.twig', [
-                'temporadas' => false
-            ]);
-        } else {
-
-            $this->get('translator')->trans('layout.inicio');
-
-            return $this->render('pagina/hojiblanca.html.twig', [
-                'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-                'locale' => $request->getLocale(),
-            ]);
-        }
-    }
-
-    /**
-     * @Route("/lechin", name="lechin")
-     *
-     */
-    public function lechinAction(Request $request)
-    {
-        if ($this->get('security.authorization_checker')->isGranted('ROLE_USUARIO')) {
-            return $this->render('default/principal.html.twig', [
-                'temporadas' => false
-            ]);
-        } else {
-
-            $this->get('translator')->trans('layout.inicio');
-
-            return $this->render('pagina/lechin.html.twig', [
-                'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
-                'locale' => $request->getLocale(),
+                'titulo' => $titulo,
+                'cuerpo' => $cuerpo
             ]);
         }
     }
